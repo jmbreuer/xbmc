6f15c7ae39b17cfe2d90256517cf8dfd583d28c5
Joachim Breuer <git@jmbreuer.net>
2013-11-08 14:34:27 +0100

    Add skin to packaging for OE

--- packageForOE.sh
+++ packageForOE.sh
@@ -9,3 +9,7 @@ popd
 pushd lib
 pax -w -s !ffmpeg!ffmpeg-jmbreuer! ffmpeg > /tmp/ffmpeg-jmbreuer.tar
 popd
+
+pushd addons
+pax -w -s !skin.confluence!xbmc-theme-Confluence-jmbreuer! skin.confluence > /tmp/xbmc-theme-Confluence-jmbreuer.tar
+popd
