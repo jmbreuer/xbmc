17c08f14b47d811406f26de565c9bd5520bc9c67
Joachim Breuer <git@jmbreuer.net>
2023-04-25 21:35:32 +0200

    Implement subtitle 24/25fps frame rate correction
    Time-stretch subtitle pts to sync 24fps subtitles with 25fps mastered
    video and vice versa.

--- xbmc/application/ApplicationPlayer.h
+++ xbmc/application/ApplicationPlayer.h
@@ -97,8 +97,9 @@ public:
   std::string GetPlayerState();
   KODI::PLAYLIST::Id GetPreferredPlaylist() const;
   int GetSubtitleDelay() const;
+  ESUBTITLESTRETCH GetSubtitleStretch() const;
   int GetSubtitle();
-  void GetSubtitleCapabilities(std::vector<int>& subCaps) const;
+  void GetSubtitleCapabilities(std::vector<IPlayerSubtitleCapabilities>& subCaps) const;
   int GetSubtitleCount() const;
   void GetSubtitleStreamInfo(int index, SubtitleStreamInfo& info) const;
   bool GetSubtitleVisible() const;
@@ -154,6 +155,7 @@ public:
   bool SetPlayerState(const std::string& state);
   void SetSubtitle(int iStream);
   void SetSubTitleDelay(float fValue = 0.0f);
+  void SetSubtitleStretch(ESUBTITLESTRETCH value);
   void SetSubtitleVisible(bool bVisible);
 
   /*!
